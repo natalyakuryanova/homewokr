087723c46ee375441b2b4ec74f48d949f136f5eb
Kuryanova Natalya <natalyakuryanova@mail.ru>
2023-09-07 16:18:48 +0300

    дз №64,66,68

---
+++ homework_66/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов 
+в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+Console.Clear();
+int Meaning(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int SumOfElements(int n, int m)
+{
+  if (n == m) return n;
+  else return SumOfElements(n + 1, m) + n;
+}
+
+int n = Meaning("Введите значение N: ");
+int m = Meaning("Введите значение M: ");
+
+Console.WriteLine(SumOfElements(n, m));
---
+++ homework_68/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29 */
+
+Console.Clear();
+int Meaning(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int AkkermanFunk(int m, int n)
+{
+  if (m == 0) return n + 1;
+  if (m > 0 && n == 0) return AkkermanFunk(m - 1, 1);
+  else return AkkermanFunk(m - 1, AkkermanFunk(m, n - 1));
+}
+
+int m = Meaning("Введите значение M: ");
+int n = Meaning("Введите значение N: ");
+
+Console.WriteLine($"A({m},{n}) = {AkkermanFunk(m, n)}");
